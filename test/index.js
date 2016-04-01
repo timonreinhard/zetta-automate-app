ce8e4cea5ed0c9afb491529be2f4347eb02746e0
Timon Reinhard <hallo@timonreinhard.de>
2016-04-01 21:12:48 +0200

    Add interface for accessing yml-based rule sets

---
+++ test/index.js
@@ -0,0 +1,66 @@
+var expect = require('must');
+
+var Rules = require('../lib/rules');
+
+/* eslint-env mocha */
+
+describe('Rules', function() {
+
+  var rules;
+
+  beforeEach(function(done) {
+    rules = new Rules();
+    rules.load(__dirname + '/fixture.yml', function(err, result) {
+      expect(err).to.be.falsy();
+      expect(result).to.have.length(3);
+      done();
+    });
+  });
+
+  describe('.get(id)', function() {
+    it('should return rule with given id', function() {
+      var result = rules.get('flood-the-basement');
+      expect(result).to.exist();
+    });
+  });
+
+  describe('.getByTrigger(devicename, state)', function() {
+    it('should return rules that match a devicename', function() {
+      var result = rules.getByTrigger('Red Light');
+      expect(result).to.have.length(2);
+      expect(result[0]).to.have.property('id', 'tv-mood');
+      expect(result[1]).to.have.property('id', 'flood-the-basement');
+    });
+
+    it('should return rules that match a devicename and state', function() {
+      var result = rules.getByTrigger('Red Light', 'on');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('id', 'flood-the-basement');
+    });
+  });
+
+});
+
+describe('Rule', function() {
+
+  var rule;
+
+  beforeEach(function(done) {
+    var rules = new Rules();
+    rules.load(__dirname + '/fixture.yml', function(err, result) {
+      if (err) throw err;
+      rule = result[0];
+      done();
+    });
+  });
+
+  describe('.getActions()', function() {
+    it('should return actions for the rule', function() {
+      var result = rule.getActions();
+      expect(result).to.have.length(2);
+      expect(result[0]).to.have.property('device', 'backlight');
+      expect(result[1]).to.have.property('action', 'do nothing');
+    });
+  });
+
+});
